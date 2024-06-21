da6782fa38b802af8fcc64f26fec7bcbf3d84d48
lafkpages <luisafk@proton.me>
2024-06-21 21:06:54 +0200

    chore: comments explaining password1 and password2 files

--- src/tests/dummy/index.ts
+++ src/tests/dummy/index.ts
@@ -1,6 +1,14 @@
 import rawPasswordOne from "./password1.txt";
 import rawPasswordTwo from "./password2.bin";
 
+// rawPasswordOne is configured in bunfig.toml to
+// be loaded as text, so it should be bundled into
+// the binary as a string.
+
+// rawPasswordTwo is configured to be loaded as a file,
+// so it should be included as a separate file in
+// the binary bundle.
+
 const password =
   rawPasswordOne.trim() + (await Bun.file(rawPasswordTwo).text()).trim();
 
