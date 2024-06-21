dc71b943cd802a7e9944f8037eff626d7c4fe099
lafkpages <luisafk@proton.me>
2024-06-21 18:16:13 +0200

    feat: make into library, export main function

---
+++ src/tests/index.test.ts
@@ -0,0 +1,14 @@
+import { extractBundledFiles } from "..";
+import { test, expect } from "bun:test";
+
+const dummy = Bun.file("dist/dummy");
+const dummyData = Buffer.from(await dummy.arrayBuffer());
+
+test("extractBundledFiles with dummy executable", () => {
+  const bundledFiles = extractBundledFiles(dummyData);
+
+  expect(bundledFiles).toHaveLength(2);
+
+  expect(bundledFiles[0].path).toEndWith("/dummy");
+  expect(bundledFiles[1].path).toEndWith(".bin");
+});
