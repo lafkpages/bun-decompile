1cb6b16b1f2845ee010a83b5048dec59b7e01d07
lafkpages <luisafk@proton.me>
2024-08-27 03:52:38 +0900

    test: selfcheck compare Bun.embeddedFiles with extractBundledFiles

--- src/lib/tests/selfcheck/index.ts
+++ src/lib/tests/selfcheck/index.ts
@@ -1,10 +1,12 @@
+import type { BundledFile } from "../..";
+
 import { equal, ok } from "node:assert/strict";
 
 import { extractBundledFiles } from "../..";
 
-// TODO: compare bundledFiles with Bun.embeddedFiles
+console.log("\n\nSelfcheck!");
 
-console.debug(process.execPath);
+console.debug("Selfchecking", process.execPath);
 
 const bundledFiles = extractBundledFiles(await Bun.file(process.execPath).arrayBuffer());
 
@@ -12,8 +14,21 @@ console.debug(bundledFiles, Bun.embeddedFiles);
 
 ok(Bun.embeddedFiles.length === 3);
 
-equal(await Bun.embeddedFiles[0].text(), "hi\n");
-equal(await Bun.embeddedFiles[1].text(), "again\n");
-equal(await Bun.embeddedFiles[2].text(), "third asset\n");
+const decoder = new TextDecoder();
+
+const bundledFilePaths = new Map<string, BundledFile>();
+for (const bundledFile of bundledFiles) {
+  bundledFilePaths.set(bundledFile.path, bundledFile);
+}
+
+for (const embeddedFile of Bun.embeddedFiles) {
+  const bundledFile = bundledFilePaths.get(embeddedFile.name);
+
+  // ensure all embedded files are in the bundled files
+  ok(bundledFile);
+
+  // ensure file contents match
+  equal(decoder.decode(bundledFile.contents), await embeddedFile.text());
+}
 
 console.info("Selfcheck passed!");
